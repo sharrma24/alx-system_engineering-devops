b29199fd9be3cde3445539a91847d09433b47e07
Sharma <sharmaisse@gmail.com>
2024-05-02 09:06:34 +0000

    pp config

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Using puppet, create below files with below specifications
+
+file { '/tmp/holberton':
+  ensure  => file,
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
